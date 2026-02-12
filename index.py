d4267cc94d17afb91cc43a2ad0ca8c01a99661b3
awab07 <awab477737@gmail.com>
2026-02-12 20:55:06 +0500

    update the code

--- index.py
+++ index.py
@@ -1,10 +1,74 @@
-num=2
-num2=3
-print(num + num2 )
+# num=2
+# num2=3
+# print(num + num2 )
 
 
-name="Awab"
-print(name)
+# name="Awab"
+# print(name)
 
-numfirst=3
-print(numfirst)
+# numfirst=3
+# print(numfirst)
+
+# n = "45 Nabeel"
+# print(n,type(n))
+
+# type casting
+
+# number= input("enter your number: ")
+# print(number,type(number))
+# number=int(number)
+# print(number,type(number))
+
+# number= int(input("enter your number: "))
+# print(number,type(number))
+
+# number= float(input("enter your number: "))
+# print(number,type(number))
+
+# number=44
+# if number=="44":
+#     print("yes you are right")
+# else:
+#     print("enter wrong value")
+
+# marks=99
+# if marks==90:
+#     print("yes you are passed")
+# elif marks==99:
+#     print("hurry ! you are passed in good number")
+# else:
+#     print("enter wrong value")
+
+
+
+# edu = int(input("Enter your Education: "))
+# hei = float(input("Enter your Height: "))
+# aGe = int(input("Enter your Age: "))
+
+# if edu == 12 and hei == 5.7 and aGe == 18:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# if aGe>17 and edu>11:
+#     print("pass")
+# elif edu>11 and hei>5.6 :
+#     print("pass")
+# elif aGe>17 and hei>5.6:
+#     print("pass")
+# else:
+#     print("fail")
+
+# if (aGe>17 and edu>11) or (edu>11 and hei>5.6) or (aGe>17 and hei>5.6):
+#     print("pass")
+# else:
+#     print("fail")
+
+# num=10
+# match(num):
+#     case 0:
+#         print("none")
+#     case 1:
+#         print("Ok")
+#     case _1:
+#         print("hello")
